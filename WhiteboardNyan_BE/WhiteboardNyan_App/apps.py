e0c16a4db274cc9894bf885e2ddb59269369e472
Chatchai222 <65013350+Chatchai222@users.noreply.github.com>
2022-10-14 00:23:03 +0700

    Refactored urls to WhiteboardNyan_App and added sample.html (#5)

--- WhiteboardNyan_BE/WhiteboardNyan_App/apps.py
+++ WhiteboardNyan_BE/WhiteboardNyan_App/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class WhiteboardnyanAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "WhiteboardNyan_BE.WhiteboardNyan_App"
+
